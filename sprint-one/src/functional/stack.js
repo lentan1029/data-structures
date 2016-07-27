283fdafbbc92475d95bd1f88a22bd579a79e8268
Damien Rochford <djrochford@gmail.com>
2016-07-27 12:04:41 -0700

    Pass First Three Functional Pattern Stack Tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,20 +1,21 @@
 var Stack = function() {
   var someInstance = {};
+  
+  var size = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
-    size++
+    size++;
   };
 
   someInstance.pop = function() {
   };
 
   someInstance.size = function() {
-    return 0;
+    return size;
   };
 
   return someInstance;
