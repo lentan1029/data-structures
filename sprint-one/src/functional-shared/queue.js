9f03b2b8a373bc2904736ca47d487f6b88de8ead
lentan1029 <lentan1029@gmail.com>
2016-07-27 15:27:45 -0700

    added size method for functional-shared queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,17 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  newInstance = {};
+  newInstance.length = 0;
+  newInstance.storage = {};
+
+  _.extend(newInstance, queueMethods);
+
+  return newInstance;
 };
 
 var queueMethods = {};
 
-
+queueMethods.size = function() {
+  return this.length;
+};
