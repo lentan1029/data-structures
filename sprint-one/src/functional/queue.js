c6e7dc336a75eab5729d016a15f8629a527a896b
Damien Rochford <djrochford@gmail.com>
2016-07-27 12:41:46 -0700

    Pass All Functional Pattern Queue Tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -9,10 +9,17 @@ var Queue = function() {
 
   someInstance.enqueue = function(value) {
     size++;
+    storage[size-1] = value;
   };
 
   someInstance.dequeue = function() {
+    var output = storage[0];
+    for (i=1; i<size; i++) {
+      storage[i-1] = storage[i];
+    }
+    delete storage[size-1];
     size--;
+    return output;
   };
 
   someInstance.size = function() {
