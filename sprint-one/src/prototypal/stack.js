c1506f29acb3714d9429370fb77dd4d633120bc9
lentan1029 <lentan1029@gmail.com>
2016-07-27 15:56:13 -0700

    Pass first Stack Prototypical Test

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,16 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newInstance = Object.create(stackMethods);
+
+  newInstance.length = 0;
+  newInstance.storage = {};
+
+  return newInstance;
 };
 
 var stackMethods = {};
 
-
+stackMethods.size = function() {
+  return this.length;
+};
