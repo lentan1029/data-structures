d62220ab8ad12d3e0de775217151cac365aff1f9
lentan1029 <lentan1029@gmail.com>
2016-07-27 16:03:27 -0700

    Pass First Prototypical Queue Test

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,17 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newInstance = Object.create(queueMethods);
+  newInstance.length = 0;
+  newInstance.storage = {};
+
+  return newInstance;
 };
 
 var queueMethods = {};
+
+queueMethods.size = function() {
+  return this.length;
+};
 
 
