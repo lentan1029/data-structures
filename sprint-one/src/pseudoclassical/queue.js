b94361ed36bd76a19e5ceed9d9df8f011980aabd
lentan1029 <lentan1029@gmail.com>
2016-07-27 16:18:09 -0700

    passed first test for psuedoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,11 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.length = 0;
+  this.storage = {};
+
 };
 
-
+Queue.prototype.size = function() {
+  return this.length;
+};
