6f07353951b911b6ac8a5d5dbb42230c92337f39
lentan1029 <lentan1029@gmail.com>
2016-07-27 19:37:24 -0700

    Pass All Tree Tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -20,6 +20,26 @@ treeMethods.addChild = function(value) {
 };
 
 treeMethods.contains = function(target) {
+
+  var res = false;
+  
+  var checkTree = function(tree) {
+    if (tree.value === target) {
+      res = true;
+    }
+  };
+
+  var traverseTree = function(tree) {
+    checkTree(tree);
+    for (var i = 0; i < tree.children.length; i++) {
+      traverseTree(tree.children[i]);
+    }
+  };
+
+  traverseTree(this);
+
+
+  return res;
 };
 
 
