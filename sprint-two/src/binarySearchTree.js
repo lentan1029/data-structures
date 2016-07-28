aafda51cfa332c835e9a9ea1fe0ffc668d9de89c
lentan1029 <lentan1029@gmail.com>
2016-07-28 11:15:14 -0700

    Pass First BST Test

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,9 +1,18 @@
 var BinarySearchTree = function(value) {
-  this.value = value;
-  this.left = null;
-  this.right = null;
+  var newTree = Object.create(BinarySearchTree.prototype);
+
+  newTree.value = value;
+  newTree.left = null;
+  newTree.right = null;
+
+  return newTree;
 };
 
+BinarySearchTree.prototype.insert = function() {};
+
+BinarySearchTree.prototype.contains = function() {};
+
+BinarySearchTree.prototype.depthFirstLog = function() {};
 
 /*
  * Complexity: What is the time complexity of the above functions?
