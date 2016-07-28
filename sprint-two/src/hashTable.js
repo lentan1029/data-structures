abea20b2a040063ff37b0ee104f3116217775fb7
lentan1029 <lentan1029@gmail.com>
2016-07-27 22:08:58 -0700

    Progress on Hashtable; Key Insight in Comment

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,12 +7,21 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+  if (this._storage.get(index)) {
+    var oldStorage = this._storage;
+    this._limit = this._limit * 2;
+    this._storage = LimitedArray(this._limit);
+    oldStorage.each(function(item, index, collection) {
+      this.insert(item); //what if we collide again and at line 11 we don't copy over all the items in the old collection
+    });
+  }
+  this._storage.set(index, {k: v});
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  return this._storage.get(index) ? this._storage.get(index)['k'] : undefined;
 };
 
 HashTable.prototype.remove = function(k) {
@@ -20,7 +29,7 @@ HashTable.prototype.remove = function(k) {
   this._storage.set(index, undefined);
 };
 
-
+// if there isn't any collision for limit n, there won't be for limit 2*n.
 
 /*
  * Complexity: What is the time complexity of the above functions?
