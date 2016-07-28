8a0b397d422db82499fedc5d1bfb91b52dba0171
lentan1029 <lentan1029@gmail.com>
2016-07-27 20:06:29 -0700

    Implement Node Removal in Graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -16,14 +16,23 @@ Graph.prototype.contains = function(node) {
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  for (var i = 0; i < this[node].length; i++) {
+    var neighbor = this[this[node][i]];
+    var index = neighbor.indexOf(node);
+    neighbor.splice(index, 1);
+  }
+  delete this[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return this[fromNode].indexOf(toNode) !== -1;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this[fromNode].push(toNode);
+  this[toNode].push(fromNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
